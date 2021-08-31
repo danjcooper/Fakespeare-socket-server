ec4ebdc20cefa670547db17c8bbc53c3c58e72a6
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-31 21:56:13 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+// const { SocketAddress } = require('net');
+const httpServer = require('http').createServer();
+
+const port = process.env.PORT || 3000;
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('hello, new connection');
+
+  socket.on('joinRoom', (data) => {
+    socket.join(data.roomName);
+    console.log(`${data.username} joined ${data.roomName}`);
+    // Tell everyone a user joined
+    socket.in(data.roomName).emit('joinRoom', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Someone disconnected from the socket..');
+  });
+});
+
+httpServer.listen(port, () =>
+  console.log(`Express now departing from port ${port}!`)
+);
