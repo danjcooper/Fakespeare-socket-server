ac9a289e493542e48ffe793f22e75a81324c0890
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-09-15 17:59:58 +0100

    update

--- index.js
+++ index.js
@@ -11,13 +11,23 @@ const io = require('socket.io')(httpServer, {
 });
 
 io.on('connection', (socket) => {
-  console.log('hello, new connection');
+  console.log('hello, new connection ');
 
   socket.on('joinRoom', (data) => {
     socket.join(data.roomName);
     console.log(`${data.username} joined ${data.roomName}`);
     // Tell everyone a user joined
     socket.in(data.roomName).emit('newPlayer', data);
+  });
+
+  socket.on('player update', (data) => {
+    console.log(data.playerList);
+    socket.in(data.roomName).emit('updated players', data.playerList);
+  });
+
+  socket.on('start game', (data) => {
+    console.log(data.roomCode);
+    socket.in(data.roomCode).emit('start game');
   });
 
   socket.on('disconnect', () => {
